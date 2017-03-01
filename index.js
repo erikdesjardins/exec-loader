8d77d9c6ec121ef2f572ece15a39f149355ea9c3
Erik <erikdesjardins@users.noreply.github.com>
2017-03-01 14:14:26 -0500

    don't cache results by default, add cache option

--- index.js
+++ index.js
@@ -5,7 +5,13 @@
 
 'use strict';
 
+var loaderUtils = require('loader-utils');
+
 module.exports = function(source) {
+	var options = Object.assign({ cache: false }, loaderUtils.getOptions(this));
+
+	this.cacheable(options.cache);
+
 	var result = this.exec(source, this.resourcePath);
 	return 'module.exports = ' + JSON.stringify(result) + ';';
 };
